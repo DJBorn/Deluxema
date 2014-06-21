35b0c58f0ea8d7d25a590fe540f267756d317da3
Daniel Jung <ilgoojung@hotmail.com>
2014-06-20 22:59:15 -0400

    Documented Explosion
    - Added documentation to the Explosion class and revised some redundant
    code

--- Deluxema/Explosion.cpp
+++ Deluxema/Explosion.cpp
@@ -24,22 +24,28 @@ Explosion::Explosion()
 	on = false;
 	maxDelay = 4;
 	delay = maxDelay - 1;
+
+	// Set the frame in which the next explosion will begin to play
 	followFrame = 3;
 	int volume = 87;
 
+	// Create the first sprite
 	startSprite = new sprite;
 	startSprite->curFrame = 0;
 	startSprite->id = generateid();
 	startSprite->activated = false;
 	startSprite->explosionSound = new Sound("includes//Sounds//Effects//Explosion.wav", volume);
 
+	// Remember the previous sprite to link it to its child
 	sprite* previousSprite = startSprite;
 
+	// Load the sprite with the struct data
 	dbCreateAnimatedSprite(startSprite->id,"includes//Sprites//Effects//Explosion.bmp", 4, 2, startSprite->id);
 	dbScaleSprite(startSprite->id, 200);
 	dbSetSpritePriority(startSprite->id, 201);
 	dbHideSprite(startSprite->id);
 
+	// Create 9 more sprites. (The explosion animation has 8 frames, so 10 sprites should be plenty)
 	for(int i = 1; i < 10 ; i++)
 	{
 		sprite* newSprite;
@@ -63,6 +69,7 @@ Explosion::Explosion()
 }
 Explosion::~Explosion()
 {
+	// Delete all sprites
 	sprite* workingSprite;
 	workingSprite = startSprite;
 	for(int i = 0; i < 10; i++)
@@ -93,25 +100,37 @@ void Explosion::playExplosion(int X1, int X2, int Y1, int Y2)
 		delay = 0;
 		if(on)
 		{
+			// Create a pointer to the currently working sprite
 			sprite* workingSprite;
+
+			// Set the working sprite as the first sprite in the explosion
 			workingSprite = startSprite;
 			for(int i = 0; i < 10; i++)
 			{
+				// The current sprite must be active
 				if(workingSprite->activated)
 				{
+					// Increment the frame
 					workingSprite->curFrame++;
+
+					// If it has just begun, play the explosion sound and randomize its location
 					if(workingSprite->curFrame == 1)
 					{
 						workingSprite->explosionSound->playSound();
 						workingSprite->locationX = dbRnd(X2 - X1) + (X1 - 23);
 						workingSprite->locationY = dbRnd(Y2 - Y1) + (Y1 - 22);
 					}
+					// If the current frame is the followFrame, then activate its child to play
 					if(workingSprite->curFrame == followFrame)
 					{
 						workingSprite->child->activated = true;
+						// If this is the last sprite in the list, then play the first sprite again on this iteration
+						// (This is because all the other sprites would've been able to play atleast once
+						//  on the iteration they were activated in EXCEPT the first one)
 						if(i == 9)
 							i--;
 					}
+					// If it has reached its final frame, deactivate the sprite, reset its frame, and hide the sprite
 					if(workingSprite->curFrame > 8)
 					{
 						workingSprite->curFrame = 0;
@@ -124,11 +143,15 @@ void Explosion::playExplosion(int X1, int X2, int Y1, int Y2)
 						dbShowSprite(workingSprite->id);
 					}
 				}
+				// update the sprite on the screen
+				dbSprite(workingSprite->id, workingSprite->locationX, workingSprite->locationY, workingSprite->id);
+				// set the next working sprite as the child
 				workingSprite = workingSprite->child;
 			}
 		}
 		else
 		{
+			// If the explosion has turned off, then repeat steps above except do not activate on the follow frame
 			sprite* workingSprite;
 			workingSprite = startSprite;
 			for(int i = 0; i < 10; i++)
@@ -153,17 +176,10 @@ void Explosion::playExplosion(int X1, int X2, int Y1, int Y2)
 						dbShowSprite(workingSprite->id);
 					}
 				}
+				dbSprite(workingSprite->id, workingSprite->locationX, workingSprite->locationY, workingSprite->id);
 				workingSprite = workingSprite->child;
 			}
 		}
-	}
-	sprite* workingSprite;
-	workingSprite = startSprite;
-	for(int i = 0; i < 10; i++)
-	{
-		// position the sprite and update it
-		dbSprite(workingSprite->id, workingSprite->locationX, workingSprite->locationY, workingSprite->id);
-		workingSprite = workingSprite->child;
 	}
 }
 
--- Deluxema/Explosion.h
+++ Deluxema/Explosion.h
@@ -5,19 +5,39 @@
 
 using namespace std;
 
+/* This is a class that handles continuous explosions, it consists of
+   of one sprite sheet that is created multiple times. The sprite will
+   play a single explosion animation, and as that sprite enters a specific
+   frame, it will start another explosion animation. This will happen 
+   continuously until the explosion is turned off. The location of the
+   sprites depend on a specific x1, x2, y1, and y2 locations that will
+   select where the explosion will appear randomly within those coordinates.
+   Each sprite is created as a struct and to communicate with each other, they
+   will be put into a linked cycle list */
 class Explosion
 {
 public:
+	// Constructors and Destructors
 	Explosion();
 	~Explosion();
 	
+	// Turns on the explosion
 	void turnOnExplosion();
+
+	// Play the explosion within these coordinates
 	void playExplosion(int X1, int X2, int Y1, int Y2);
+
+	// Turn off the explosion
 	void turnOffExplosion();
+
+	// Reset the explosion and all of its variables
 	void resetExplosion();
+
+	// Fade the sound of the explosions
 	void fadeSound();
 
 private:
+	// A structure that holds a single explosion sprite
 	struct sprite
 	{
 		bool activated;
@@ -28,10 +48,17 @@ private:
 		sprite* child;
 		Sound* explosionSound;
 	};
+
+	// The first sprite in the linked list
 	sprite* startSprite;
+
+	// The frame in which the next sprite in the list should begin playing
 	int followFrame;
+
+	// Flag to determine if the explosion is on
 	bool on;
-	int x1, x2, y1, y2;
+
+	// delay variables
 	int delay;
 	int maxDelay;
 };
